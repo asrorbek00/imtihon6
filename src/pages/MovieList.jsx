692075669ef4757fd02e480b27caa5446aaabeba
asrorbek00 <aserravshanbekov@gmail.com>
2023-10-11 08:16:58 +0500

    m

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -18,7 +18,7 @@ const MovieList = () => {
     isPending,
     error,
   } = useFetch(`
-    https://api.themoviedb.org/3/discover/movie/?api_key=3d820eab8fd533d2fd7e1514e86292ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=$`);
+    https://api.themoviedb.org/3/discover/movie/?api_key=3d820eab8fd533d2fd7e1514e86292ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=`);
 
 
   return (<>
